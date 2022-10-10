c8d34615f7b9d6378f20320956f6027435565dca
TruongMai <112840965+TruongMai98@users.noreply.github.com>
2022-10-10 10:34:50 +0700

    xoa phan tu mang

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double[] numArr = new double[10];
+        for (int i = 0; i < numArr.length; i++) {
+            numArr[i] =  Math.floor(Math.random() * 10);
+            System.out.print(numArr[i] + " ");
+        }
+        System.out.println("\n");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nhap so can xoa");
+        double xDel = scanner.nextDouble();
+        for (int i = 0; i < numArr.length; i++) {
+            if (xDel == numArr[i]) {
+                for (int j = i; j < numArr.length -1; j++) {
+                    numArr[i] = numArr[i + 1];
+                }
+            }
+            System.out.print(numArr[i] + " ");
+        }
+
+
+
+    }
+}
